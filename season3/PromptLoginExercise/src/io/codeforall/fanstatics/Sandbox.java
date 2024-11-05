bba051eb24e671fc6aeed707edcbd240a1601145
Leandro Goncalves <leandrogoncalves101395@gmail.com>
2024-11-05 23:43:14 +0000

    small update prompt exercise

--- season3/PromptLoginExercise/src/io/codeforall/fanstatics/Sandbox.java
+++ season3/PromptLoginExercise/src/io/codeforall/fanstatics/Sandbox.java
@@ -1,4 +1,19 @@
 package io.codeforall.fanstatics;
 
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
+import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+
 public class Sandbox {
+    public static void main(String[] args) {
+
+        String UsernameQ = new String("Username:");
+        String PasswordQ = new String("Password:");
+        Prompt prompt = new Prompt(System.in, System.out);
+
+        StringInputScanner var3 = new StringInputScanner();
+
+        var3.setMessage("Login");
+        System.out.println(UsernameQ.);
+    }
 }
